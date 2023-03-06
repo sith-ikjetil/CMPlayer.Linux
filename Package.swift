1982d7ae7e1f118a899245ab7e1ffa47358c554d
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-03-06 21:52:22 +0100

    Updated.

--- Package.swift
+++ Package.swift
@@ -3,11 +3,14 @@ import PackageDescription
 
     let package = Package(
         name: "CMPlayer",
+        products: [
+            .library(name: "Cmpg123", targets: ["Cmpg123"]),
+        ],
         dependencies: [
             .package(url: "https://github.com/ponyboy47/Termios.git", from: "0.1.1"),            
-        ],
-        targets: [
-            .systemLibrary(name: "Cmpg123"),
+        ],        
+        targets: [    
+            .systemLibrary(name: "Cmpg123"),        
             .executableTarget(name: "CMPlayer", dependencies: ["Termios", "Cmpg123"]),            
         ]
     )
