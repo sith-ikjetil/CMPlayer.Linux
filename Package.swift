498a99e79c0e7ebd27d3ed21a3ad49a80f2f379d
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-03-07 09:39:05 +0100

    Updated.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 // swift-tools-version: 5.7.3
 import PackageDescription
+    let pkgConfig = "libmpg123-0"
+    
+    let provider: [SystemPackageProvider] = [ 
+        .apt(["libmpg123"])
+    ]   
 
     let package = Package(
         name: "CMPlayer",
@@ -10,7 +15,7 @@ import PackageDescription
             .package(url: "https://github.com/ponyboy47/Termios.git", from: "0.1.1"),            
         ],        
         targets: [    
-            .systemLibrary(name: "Cmpg123"),        
+            .systemLibrary(name: "Cmpg123", path: "Sources/Cmpg123", pkgConfig: pkgConfig, providers: provider),        
             .executableTarget(name: "CMPlayer", dependencies: ["Termios", "Cmpg123"]),            
         ]
     )
