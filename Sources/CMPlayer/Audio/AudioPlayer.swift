36cfcb7b44a009bddab73a33768170b4ed48a9c3
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2024-08-17 17:46:58 +0200

    Added AudioPlayer

---
+++ Sources/CMPlayer/Audio/AudioPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  AudioPlayer.swift
+//  ConsoleMusicPlayer-macOS
+//
+//  Created by Kjetil Kr Solberg on 21/09/2019.
+//  Copyright © 2019 Kjetil Kr Solberg. All rights reserved.
+//
+
+//
+// import.
+//
+import Foundation
+import Cmpg123
+
+//
+// Represents CMPlayer AudioPlayer.
+//
+internal class XAudioPlayer {
+    var ix: Int32 = 0;
+}// AudioPlayer
