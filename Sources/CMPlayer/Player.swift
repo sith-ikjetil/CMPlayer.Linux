75ca30ad3473887e1f2f27941e784ab89bd64d24
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2024-08-23 19:02:06 +0200

    rollback of prev bug fix

--- Sources/CMPlayer/Player.swift
+++ Sources/CMPlayer/Player.swift
@@ -244,10 +244,7 @@ internal class Player {
                     else {
                         self.audio2!.setCrossfadeVolume(volume: 0.0, fadeDuration: UInt64(PlayerPreferences.crossfadeTimeInSeconds*1000) )
                     }
-                }
-                else {
-                    self.audio2!.stop()
-                }
+                }                
             }
             if play {
                 self.play(player: 1, playlistIndex: 0)
@@ -262,10 +259,7 @@ internal class Player {
                     else {
                         self.audio1!.setCrossfadeVolume(volume: 0.0, fadeDuration: UInt64(PlayerPreferences.crossfadeTimeInSeconds*1000) )
                     }
-                }
-                else {
-                    self.audio1!.stop()
-                }
+                }                
             }
             if play {
                 self.play(player: 2, playlistIndex: 0)
