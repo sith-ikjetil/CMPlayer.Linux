db4db9414511079b5aa6540c56aa861696d42da9
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2024-08-23 15:12:46 +0200

    Changed the way Cffmpeg works

--- Sources/Cffmpeg/Cffmpeg.c
+++ Sources/Cffmpeg/Cffmpeg.c
@@ -1,5 +1,5 @@
-#include "include/libavformat/avformat.h"
-#include "include/libavcodec/avcodec.h"
-#include "include/libswresample/swresample.h"
-#include "include/libavutil/opt.h"
-#include "include/libavutil/dict.h"
+#include </usr/include/x86_64-linux-gnu/libavformat/avformat.h>
+#include </usr/include/x86_64-linux-gnu/libavcodec/avcodec.h>
+#include </usr/include/x86_64-linux-gnu/libswresample/swresample.h>
+#include </usr/include/x86_64-linux-gnu/libavutil/opt.h>
+#include </usr/include/x86_64-linux-gnu/libavutil/dict.h>
