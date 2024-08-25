2f2c9ab27760fa8ba2a9ce843bbaa87b9e0b9644
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2024-08-25 12:51:40 +0200

    Added Casound

---
+++ Sources/Casound/Casound.c
@@ -0,0 +1,1 @@
+#include "include/bridging-header.h"
---
+++ Sources/Casound/include/bridging-header.h
@@ -0,0 +1,9 @@
+#ifdef CMP_TARGET_UBUNTU_V22_04
+#include </usr/include/alsa/asoundlib.h>
+#elif CMP_TARGET_UBUNTU_V24_04
+#include </usr/include/alsa/asoundlib.h>
+#elif CMP_TARGET_FEDORA_V40
+#include </usr/include/alsa/asoundlib.h>
+#elif CMP_TARGET_MANJARO_V24
+#include </usr/include/alsa/asoundlib.h>
+#endif
