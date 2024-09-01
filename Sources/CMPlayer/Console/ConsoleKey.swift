eea0843ef32976c0c5d046217521ada2fee67c60
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2024-09-01 15:29:08 +0200

    Added comments

--- Sources/CMPlayer/Console/ConsoleKey.swift
+++ Sources/CMPlayer/Console/ConsoleKey.swift
@@ -1,25 +1,25 @@
 //
 //  ConsoleKey.swift
 //
+//  (i): Code that identifies console key press codes.
+//
 //  Created by Kjetil Kr Solberg on 24-09-2024.
 //  Copyright © 2024 Kjetil Kr Solberg. All rights reserved.
 //
-
 //
 // import.
 //
 import Foundation
-
 //
 // Console Keys
 //
 internal enum ConsoleKey : UInt32 {
     case KEY_BACKSPACE = 127
     case KEY_ENTER = 10
-    case KEY_UP = 365 //65 //365
-    case KEY_DOWN = 366 //66 //366
-    case KEY_RIGHT = 367 //67 //367
-    case KEY_LEFT = 368 //68 //368
+    case KEY_UP = 365 
+    case KEY_DOWN = 366 
+    case KEY_RIGHT = 367 
+    case KEY_LEFT = 368 
     case KEY_HTAB = 9
     case KEY_SHIFT_HTAB = 390
     case KEY_SPACEBAR = 32
