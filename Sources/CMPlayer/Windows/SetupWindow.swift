d4d182972bb653dce96053d1bb626c00a163119c
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2024-09-05 01:29:46 +0200

    Improved setup window

--- Sources/CMPlayer/Windows/SetupWindow.swift
+++ Sources/CMPlayer/Windows/SetupWindow.swift
@@ -18,16 +18,19 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
     /// private constants
     ///
     private let concurrentQueue = DispatchQueue(label: "dqueue.cmp.linux.setup-window.1", attributes: .concurrent)
-    private let setupText: [String] = ["CMPlayer needs to have a path to search for music",
+    private let setupText: [String] = ["CMPlayer needs to have a path to search for music (music root path)",
                                        "In CMPlayer you can have many root paths.",
-                                       "In CMPlayer Use: add mrp <path> or: remove mrp <path> to add remove path.",
+                                       "In CMPlayer Use: add mrp <path> or: remove mrp <path> to add/remove path.",
                                        "Please enter the path to the root directory of where your music resides."]
+    private let pathInvalidText1: String = "Path given does not exist."
+    private let pathInvalidText2: String = "(Ctrl+C to exit, or enter a new path)"
     //
     // variables
     //
     var path: String = ""       // current path entered
     var cursor: String = ""     // cursor character
     var finished: Bool = false  // are we finished
+    var showPathInvalid: Bool = false   //
     ///
     /// Shows this InitialSetupWindow on screen.
     ///
@@ -52,7 +55,7 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
                     self.cursor = "_"
                 }
                 // render input
-                self.renderInput()
+                self.renderInput()            
                 // sleep for 75 ms
                 usleep(75_000)
             }
@@ -100,7 +103,9 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
             y += 1
         }
         // render input
-        self.renderInput()                
+        self.renderInput() 
+        // render path invalid message
+        self.renderPathInvalid()                        
         // goto g_cols,1
         Console.gotoXY(g_cols,1)
         // print nothing
@@ -112,6 +117,32 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
     func renderInput() {
         // render input
         Console.printXY(1,5+self.setupText.count + 1, ":> \(self.path)\(self.cursor)", g_cols, .left, " ", ConsoleColor.black, ConsoleColorModifier.none, ConsoleColor.cyan, ConsoleColorModifier.bold)
+    }
+    ///
+    /// render path invalid message
+    /// 
+    func renderPathInvalid() {
+        // create a variable to  hold the message
+        var msg1: String = ""
+        var msg2: String = ""
+        // if showPathInvalid flag is set
+        if self.showPathInvalid {
+            // add pathInvalidText1 to msg variable
+            msg1 += self.pathInvalidText1          
+            // add pathInvalidText2 to msg variable  
+            msg2 += self.pathInvalidText2
+        }
+        // else showPathInvalid flag is not set
+        else {
+            // add to msg1 variable
+            msg1 += " "
+            // add to msg2 variable
+            msg2 += " "
+        }
+        // render message 1
+        Console.printXY(4,5+self.setupText.count + 2, msg1, g_cols, .left, " ", ConsoleColor.black, ConsoleColorModifier.none, ConsoleColor.red, ConsoleColorModifier.bold)
+        // render message 2
+        Console.printXY(4,5+self.setupText.count + 3, msg2, g_cols, .left, " ", ConsoleColor.black, ConsoleColorModifier.none, ConsoleColor.red, ConsoleColorModifier.bold)
     }
     ///
     /// Runs this window keyboard input and feedback.
@@ -127,13 +158,15 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
         let keyHandler: ConsoleKeyboardHandler = ConsoleKeyboardHandler()        
         // add key handler for backspace
         keyHandler.addKeyHandler(key: ConsoleKey.KEY_BACKSPACE.rawValue, closure: { () -> Bool in
+            // make sure message is not shown
+            self.showPathInvalid = false
             // if path has value
             if self.path.count > 0 {
                 // remove last character
-                self.path.removeLast()
-                // render this window
-                self.renderWindow()
+                self.path.removeLast()                
             }
+            // render this window
+            self.renderWindow()
             // do not return from keyHandler.run()
             return false
         })
@@ -143,11 +176,17 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
                 // if path does not exist
                 if !FileManager.default.fileExists(atPath: self.path) {
                     // ignore
+                    // show invalid path message
+                    self.showPathInvalid = true
+                    // render this window
+                    self.renderWindow()
                     // do not return from keyHandler.run()
                     return false
                 }
                 // we have a path, set finished flag to true
                 self.finished = true
+                // allow async to clean up
+                usleep(100_000)
                 // append path to PlayerPreference.musicRootPath
                 PlayerPreferences.musicRootPath.append(self.path)
                 // save preferences
@@ -161,6 +200,8 @@ internal class SetupWindow : TerminalSizeHasChangedProtocol, PlayerWindowProtoco
         keyHandler.addCharacterKeyHandler(closure: { (ch: Character) -> Bool in
             // append character to path
             self.path.append(String(ch))
+            // make sure message is not shown
+            self.showPathInvalid = false
             // render this window
             self.renderWindow()
             // do not return from keyHandler.run()
